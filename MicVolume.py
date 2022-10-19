69acedf8534d6cbdeff397304a75dfd9ecaf8a3a
Masafumi Kashiwagi <masafumi.kashiwagi@nutanix.com>
2022-10-20 08:13:34 +0900

    update

--- MicVolume.py
+++ MicVolume.py
@@ -16,7 +16,7 @@ import os
 import shutil
 
 INTERVAL=10
-IGNORE_DEVICES=['ZoomAudioDevice']
+IGNORE_DEVICES=['ZoomAudioDevice','Microsoft Teams Audio']
 
 def exit_program(sig, frame):
     sys.exit(0)
@@ -32,7 +32,11 @@ def get_current_device():
 def list_input_devices():
     cmd = ["SwitchAudioSource","-t","input","-a"]
     p = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
-    return p.stdout.strip().splitlines()
+    devices=[]
+    for d in p.stdout.strip().splitlines():
+        if d not in IGNORE_DEVICES:
+            devices.append(d)
+    return devices
 
 def change_input_device(dev):
     cmd = ["SwitchAudioSource","-t","input","-s",dev]
@@ -50,9 +54,13 @@ def change_mic_volume(volume):
     return p.returncode
 
 def show_mic_volume():
-    device=get_current_device()[0:3]
     devices=list_input_devices()
-    volume=get_mic_volume()
+    device=get_current_device()[0:1]
+    if len([d for d in devices if d.startswith(device)]) > 1:
+        device=get_current_device()[0:3]
+    volume=(int)(get_mic_volume()/10)
+    if volume == 10:
+        volume = 'F'
     pid=os.getpid()
     print(f"""~~~
 {device}({volume}) | size=16
@@ -63,8 +71,7 @@ Minimize Mic Volume(F7) | bash='{sys.argv[0]}' param1=minimize_mic_volume param2
 Maximize Mic Volume(F8) | bash='{sys.argv[0]}' param1=maximize_mic_volume param2={pid} terminal=false shortcut=F8
 ---""")
     for device in devices:
-        if device not in IGNORE_DEVICES:
-            print(f"{device} | bash='{sys.argv[0]}' param1=change_input_device param2={pid} param3='{device}' terminal=false")
+        print(f"{device} | bash='{sys.argv[0]}' param1=change_input_device param2={pid} param3='{device}' terminal=false")
     sys.stdout.flush()
 
 if shutil.which("SwitchAudioSource") is None:
@@ -76,15 +83,18 @@ if len(sys.argv) >= 3:
     pid = int(sys.argv[2])
     if sys.argv[1] == "minimize_mic_volume":
         change_mic_volume(0)
+        time.sleep(0.5)
         os.kill(pid, signal.SIGUSR1)
         sys.exit(0)
     elif sys.argv[1] == "maximize_mic_volume":
         change_mic_volume(100)
         os.kill(pid, signal.SIGUSR1)
+        time.sleep(0.5)
         sys.exit(0)
     elif sys.argv[1] == "change_input_device":
         change_input_device(sys.argv[3])
         os.kill(pid, signal.SIGUSR1)
+        time.sleep(0.5)
         sys.exit(0)
 
 signal.signal(signal.SIGTERM, exit_program)
